a9d219c3bad71859f8b2f8941eab39c83a8b901f
Hassan Raza <hassan.raza@redmath.com>
2026-05-12 11:30:20 +0500

    feat: initialize ReactFacts project with Vite configuration and boilerplate files

---
+++ 1) Static Pages/12) ReactFacts project - markup/project/src/main.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+    <>
+        <img src="react-logo.png" alt="React logo" width="40px" />
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </>
+)
